36ae9f66a017dfaf03515877a30bcf08a70bd0a1
ZeroTwo <hoes.romeo@gmail.com>
2025-08-29 11:25:14 +0200

    page edit

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
 </head>
 <body>
-    
+    <header>
+        <flux:navbar>
+            <flux:nav-item href="{{ route('home') }}">Home</flux:nav-item>
+            <flux:nav-item href="{{ route('categories.show', ['category' => 'coffee']) }}">Coffee</flux:nav-item>
+            <flux:nav-item href="{{ route('categories.show', ['category' => 'tea']) }}">Tea</flux:nav-item>
+            <flux:nav-item href="{{ route('dashboard') }}">Dashboard</flux:nav-item>
+        </flux:navbar>
+    </header>
+
+    <main>
+        {{ $slot }}
+    </main>
 </body>
 </html>
